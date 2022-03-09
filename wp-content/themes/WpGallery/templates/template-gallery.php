f2decdffa6650226a7b07a0c1416a36b1bcbf7f8
lukaszscislowski <lukas.scislowski@gmail.com>
2022-03-09 22:54:09 +0100

    add responsive style css

--- wp-content/themes/WpGallery/templates/template-gallery.php
+++ wp-content/themes/WpGallery/templates/template-gallery.php
@@ -22,17 +22,24 @@
 
 
     <div class="wrapper">
-        <img src="<?php the_field('obraz1') ?>" alt="" class="img-fluid" alt="">
-        <img src="<?php the_field('obraz2') ?>" alt="" class="img-fluid" alt="">
-        <img src="<?php the_field('obraz3') ?>" alt="" class="img-fluid" alt="">
-        <img src="<?php the_field('obraz4') ?>" alt="" class="img-fluid" alt="">
-        <img src="<?php the_field('obraz4') ?>" alt="" class="img-fluid" alt="">
-        <img src="<?php the_field('obraz5') ?>" alt="" class="img-fluid" alt="">
-        <img src="<?php the_field('obraz6') ?>" alt="" class="img-fluid" alt="">
-        <img src="<?php the_field('obraz7') ?>" alt="" class="img-fluid" alt="">
-        <img src="<?php the_field('obraz9') ?>" alt="" class="img-fluid" alt="">
-        <img src="<?php the_field('obraz9') ?>" alt="" class="img-fluid" alt="">
-        <img src="<?php the_field('obraz10') ?>" alt="" class="img-fluid" alt="">
+        <div class="section_1">
+            <img src="<?php the_field('obraz1') ?>" alt="" class="img-fluid hero" alt="">
+            <img src="<?php the_field('obraz2') ?>" alt="" class="img-fluid cta cta1" alt="">
+            <img src="<?php the_field('obraz3') ?>" alt="" class="img-fluid cta cta2" alt="">
+        </div>
+        <div class="section_2">
+            <img src="<?php the_field('obraz4') ?>" alt="" class="img-fluid" alt="">
+            <img src="<?php the_field('obraz4') ?>" alt="" class="img-fluid" alt="">
+            <img src="<?php the_field('obraz5') ?>" alt="" class="img-fluid" alt="">
+            <img src="<?php the_field('obraz6') ?>" alt="" class="img-fluid" alt="">
+            <img src="<?php the_field('obraz7') ?>" alt="" class="img-fluid" alt="">
+        </div>
+        <div class="section_3">
+            <img src="<?php the_field('obraz9') ?>" alt="" class="img-fluid" alt="">
+            <img src="<?php the_field('obraz10') ?>" alt="" class="img-fluid" alt="">
+            <img src="<?php the_field('obraz11') ?>" alt="" class="img-fluid" alt="">
+            <img src="<?php the_field('obraz12') ?>" alt="" class="img-fluid" alt="">
+        </div>
     </div>
 
 </section><!-- .content-area -->
